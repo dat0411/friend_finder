b44845901f205175d43a63581488884fc0968a0c
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:26:16 +0000

    Only friendship user can access show page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,5 @@
 class FriendshipsController < ApplicationController
-  before_action :current_user_must_be_friendship_receiver, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_friendship_receiver, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_friendship_receiver
     friendship = Friendship.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
