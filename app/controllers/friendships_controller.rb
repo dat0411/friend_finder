b81be677b43bc9fee3b214a0d32627429cef5947
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:26:16 +0000

    Only friendship user can modify friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,14 @@
 class FriendshipsController < ApplicationController
+  before_action :current_user_must_be_friendship_receiver, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_friendship_receiver
+    friendship = Friendship.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == friendship.receiver
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   before_action :current_user_must_be_friendship_sender, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_friendship_sender
