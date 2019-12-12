de1f3db6517d9a02f5040a511c2f4da277801735
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:26:16 +0000

    Only group_membership user can access show page

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,5 +1,5 @@
 class GroupMembershipsController < ApplicationController
-  before_action :current_user_must_be_group_membership_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_group_membership_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_group_membership_user
     group_membership = GroupMembership.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
