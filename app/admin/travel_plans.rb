1338c4ecf5d39c2ac46b2fb011051979e802bc2d
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:24:56 +0000

    Whitelist columns in TravelPlan for ActiveAdmin

--- app/admin/travel_plans.rb
+++ app/admin/travel_plans.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register TravelPlan do
+
+ permit_params :plan_description, :start, :end, :owner_id, :status, :location_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
