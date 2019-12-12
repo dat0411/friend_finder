0e65f279537e2259561b3845b68e9bfbb5fb5ad7
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:26:16 +0000

    Only group_admin user can modify group_admin

--- app/controllers/group_admins_controller.rb
+++ app/controllers/group_admins_controller.rb
@@ -1,4 +1,14 @@
 class GroupAdminsController < ApplicationController
+  before_action :current_user_must_be_group_admin_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_group_admin_user
+    group_admin = GroupAdmin.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == group_admin.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @group_admins = GroupAdmin.all
 
