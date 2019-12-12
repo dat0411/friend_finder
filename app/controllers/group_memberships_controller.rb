31847ea4efbc1fe9f23374722053cc977ebeb884
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:26:16 +0000

    Only group_membership user can modify group_membership

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,4 +1,14 @@
 class GroupMembershipsController < ApplicationController
+  before_action :current_user_must_be_group_membership_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_group_membership_user
+    group_membership = GroupMembership.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == group_membership.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @group_memberships = GroupMembership.all
 
