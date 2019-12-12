763804fb2a9f747ccc32dd4eecc635e92ea2c62f
Raghu Betina <raghu@firstdraft.com>
2019-12-12 01:24:05 +0000

    Validate presence of travel_plans.status

--- app/models/travel_plan.rb
+++ app/models/travel_plan.rb
@@ -5,4 +5,6 @@ class TravelPlan < ApplicationRecord
 
   # Validations
 
+  validates :status, :presence => true
+
 end
--- spec/models/travel_plan_spec.rb
+++ spec/models/travel_plan_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe TravelPlan, type: :model do
     end
 
     describe "Validations" do
+
+    it { should validate_presence_of(:status) }
       
     end
 end
